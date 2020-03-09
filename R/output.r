4dc7e0e56b16a25afc0d6bd28f41dfe2f7ee548b
evanoffPNNL <50331146+evanoffPNNL@users.noreply.github.com>
2020-03-09 16:51:41 -0400

    Working through changes from meeting 3/9/20. Adding new parameters for map options, reducing shape/raster to 1 parameter each. Other misc changes.

--- R/output.r
+++ R/output.r
@@ -15,72 +15,83 @@ library(inlmisc)
 #'
 #' This function is designed to take both a shape and raster object or path and create a standardized, congruent, output map.
 #'
-#' @param shape_path Input full path string to shape
-#' @param shape_obj Pass shape object in directly instead of path
-#' @param raster_path Input full path string to raster
-#' @param raster_obj Pass raster object in directly instead of path
+#' @param shape_data Either the full path string to shape file or an SF shape object
+#' @param raster_data Either the full path string to raster file or a raster object
+#' @param raster_col Column that contains the raster object's output variable
+#' @param data_classification Possible temporary column for different map type tests
+#' @param simplify Option to reduce the number/complexity of the polygons in the shape file
+#' @param raster_band Future variable for dealing with multi band/time series rasters
 #' @param dpi Settable DPI for different print/screen formats
 #' @param output_file Output file path to save the resulting plot
-#' @param data_classification This will be used to determine the colors/units/legend items based on the type of data (ie. temperature, precip, etc)
+#' @param output_file_type Sets default file type for saving plots. Choices restricted to those in ggsave function
+#' @param map_xy_min_max Vector that describes the desired extent of the map in the form of (xmin, xmax, ymin, ymax)
+#' @param map_title Title to be displayed on the output map
+#' @param map_palette Variable to hold the type of colorscale to be used
+#' @param map_width_height_in Vector that describes the desired file size of output map image in the form of (width, height) in inches
+#' @param map_legend_title Text for the legend header
+#' @param map_x_label Label for x axis
+#' @param map_y_label Label for y axis
 #' @return A ggplot object of the resulting map
 #' @export
-create_map <- function(shape_path = NULL, shape_obj = NULL, raster_path = NULL, raster_obj = NULL, raster_col = NULL,
-                       dpi = 150, output_file = NULL, data_classification = "Temp", simplify = FALSE)
+create_map <- function(shape_data = NULL, raster_data = NULL, raster_col = NULL, data_classification = "Temp", simplify = FALSE, raster_band = 1,
+                       dpi = 150, output_file = NULL, output_file_type = "png",
+                       map_xy_min_max = c(-180, 180, -90, 90), map_title = NULL,  map_palette = "RdYlBu",
+                       map_width_height_in = c(15, 10), map_legend_title = NULL, map_x_label = "x", map_y_label = "y")
 {
-   # shape_path = "data/tm_world_borders_simpl-0.3.shp"
-   # raster_path = "data/wc2.0_10m_tavg_01.tif"
-#browser()
     error <- ""
+    ouput <- "Default error"
+
     tryCatch(
     {
         # Shape loading - if given a path, use that, else expect an object passed in
-        if(is.null(shape_path))
+        if(is.null(shape_data))
         {
-            if(!is.null(shape_obj))
-                shape <- shape_obj
-            else
-                error <- "both shape null"
+           error <- "Shape data is NULL"
         }
-        else if(is.null(shape_obj))
+        else if(class(shape_data)[1] == "sf")
         {
-            shape <- gcammaptools::import_mapdata(shape_path)
+            shape <- shape_data
+        }
+        else if(class(shape_data) == "character")
+        {
+            shape <- gcammaptools::import_mapdata(shape_data)
         }
         else
         {
-            error <- "both shape NOT null"
+          error <- "Unrecognized shape data argument."
         }
 
+        # Optional argument to simplify polygons via the simplify_mapdata function
         if(simplify)
         {
             shape <- gcammaptools::simplify_mapdata(shape)
         }
 
         # Raster loading - if given a path, use that, else expect an object passed in
-        if(is.null(raster_path))
+        if(is.null(raster_data))
         {
-            if(!is.null(raster_obj))
-                raster <- raster_obj
-            else
-                error <- "both raster null"
+          error <- "Raster data is NULL"
         }
-        else if(is.null(raster_obj))
+        else if(class(raster_data)[1] == "RasterLayer")
         {
-            raster <- rgis::import_raster(raster_path)
+          raster <- raster_data
+        }
+        else if(class(raster_data)[1] == "character")
+        {
+          raster <- rgis::import_raster(raster_data)
         }
         else
         {
-            error <- "both raster NOT null"
+          error <- "Unrecognized raster data argument."
         }
+
 
         # Compare projections and equalize if necessary
         if(raster::compareCRS(shape, raster))
             error <- "proj ok"
         else
         {
-           # raster <- raster::projectRaster(raster, crs = crs(shape))
             shape <- st_transform(shape, crs(raster))
-            compare_result <- raster::compareCRS(shape, raster)
-            error <- compare_result
         }
 
         # Crop shape - for extent changes (future
@@ -91,51 +102,43 @@ create_map <- function(shape_path = NULL, shape_obj = NULL, raster_path = NULL, 
 
         # Raster operations
         if(!is.null(raster_col))
+        {
+          # check if passed in column is in the raster
+
           raster_df <- dplyr::mutate(raster_df, value = raster_df[[raster_col]])
+        }
         else
+        {
           error <- "No raster column defined"
+        }
 
         # Raster stats/information (future)
         raster_min <- minValue(raster)
         raster_max <- maxValue(raster)
         raster_layers <- nlayers(raster)
+        raster_active_band <- raster_band
 
         # Map output variables
-        lat_min <- -90
-        lat_max <- 90
-        lon_min <- -180
-        lon_max <- 180
+        x_min <- map_xy_min_max[1]
+        x_max <- map_xy_min_max[2]
+        y_min <- map_xy_min_max[3]
+        y_max <- map_xy_min_max[4]
+        map_width <- map_width_height_in[1]
+        map_height <- map_width_height_in[2]
 
         # Build colorscale and options
         # Determine nature of data and apply appropriate color scale
         # Using manual scales for now
         # browser()
-        if(data_classification == "Temp")
-        {
-          map_palette <- "RdYlBu"
-          palette_direction <- -1
-          palette_type <- "div"
-          na_value <- "Grey"
-          map_guide <- "colourbar"
-          map_title <- "World Average Temperature"
-          legend_title <- "Temperature \u00B0C"
-          map_y_scale <-  ggplot2::scale_y_continuous(limits=c(lat_min, lat_max), expand = c(0, 0), breaks=seq(-90,90,30))
-          map_x_scale <-  ggplot2::scale_x_continuous(limits=c(lon_min, lon_max), expand = c(0, 0), breaks=seq(-180,180,30))
-          map_color_scale <-  ggplot2::scale_fill_distiller(palette = map_palette, type = palette_type, direction = palette_direction, na.value = na_value, guide = map_guide )
-        }
-        if(data_classification == "Precip")
-        {
-          map_palette <- "Purples"
-          palette_direction <- -1
-          palette_type <- "seq"
-          na_value <- "Grey"
-          map_guide <- "colourbar"
-          map_title <- "World Precipitation"
-          legend_title <- "Temperature \u00B0C"
-          map_y_scale <-  ggplot2::scale_y_continuous(limits=c(lat_min, lat_max), expand = c(0, 0), breaks=seq(-90,90,30))
-          map_x_scale <-  ggplot2::scale_x_continuous(limits=c(lon_min, lon_max), expand = c(0, 0), breaks=seq(-180,180,30))
-          map_color_scale <-  ggplot2::scale_fill_distiller(palette = map_palette, type = palette_type, direction = palette_direction, na.value = na_value, guide = map_guide )
-        }
+        palette_direction <- -1
+        palette_type <- "div"
+        na_value <- "Grey"
+        map_guide <- "colourbar"
+        map_x_scale <-  ggplot2::scale_x_continuous(limits=c(x_min, x_max), expand = c(0, 0), breaks=seq(x_min,x_max, abs(x_max - x_min)/12))
+        map_y_scale <-  ggplot2::scale_y_continuous(limits=c(y_min, y_max), expand = c(0, 0), breaks=seq(y_min,y_max, abs(y_max - y_min)/6))
+        map_color_scale <-  ggplot2::scale_fill_distiller(palette = map_palette, type = palette_type, direction = palette_direction, na.value = na_value, guide = map_guide )
+
+
         if(data_classification == "Land")
         {
           map_palette <- "Set1"
@@ -157,24 +160,21 @@ create_map <- function(shape_path = NULL, shape_obj = NULL, raster_path = NULL, 
           ggplot2::geom_sf(data = shape, na.rm = TRUE, fill=FALSE) +
           map_color_scale +
           ggplot2::coord_sf() +
-          ggplot2::labs(x="\u00B0Longitude", y="\u00B0Latitude", title = map_title, fill = legend_title) +
+          ggplot2::labs(x=map_x_label, y=map_y_label, title = map_title, fill = map_legend_title) +
           map_x_scale +
           map_y_scale +
           theme(plot.title = element_text(hjust = 0.5))
 
-         # scale_color_manual(values = wes.palette(n=3, name="GrandBudapest"))
+        # scale_color_manual(values = wes.palette(n=3, name="GrandBudapest"))
 
         # Save File
         if(!is.null(output_file))
-          ggplot2::ggsave(filename = paste0(output_file, "-no.png"), device = "png", dpi = dpi, limitsize = TRUE,
-                        width = 15, height = 10)
-        # ggplot2::ggsave(filename =paste0(output_file, ".bmp"), device = "bmp", dpi = dpi, limitsize = TRUE,
-        #                 width = 15, height = 10)
-
+          ggplot2::ggsave(filename = paste0(output_file, ".", output_file_type), device = output_file_type, dpi = dpi, limitsize = TRUE,
+                        width = map_width, height = map_height)
     },
     error = function(err)
     {
-        # error handler picks up where error was generated
+        # error handler picks up error information
         error <- err
         return(error)
     })
