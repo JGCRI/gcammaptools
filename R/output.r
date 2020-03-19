8833f4f1b9c5c90747cd22f1adb7aceb9956092e
evanoffPNNL <50331146+evanoffPNNL@users.noreply.github.com>
2020-03-19 10:02:41 -0400

    Removed file type parameter and consolidated name/type to one parameter. Other minor changes

--- R/output.r
+++ R/output.r
@@ -2,10 +2,6 @@
 #
 # This file produces output from standard functions
 
-library(RColorBrewer)
-# library(wesanderson)
-# library(inlmisc)
-
 #' Create a map object and return/save the output
 #'
 #' This function is designed to take both a shape and raster object or path and create a standardized, congruent, output map.
@@ -20,8 +16,7 @@ library(RColorBrewer)
 #' @param bins (Numeric) - Number of bins/segments in which to divide the raster
 #' @param convert_zero (Boolean) - Convert raster 0 values to NA, default FALSE
 #' @param dpi (Numeric) - Settable DPI for different print/screen formats
-#' @param output_file (Character) - Output file path to save the resulting plot
-#' @param output_file_type (Character) - Sets file extension type for saving plots. Choices restricted to ("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg" or "wmf)
+#' @param output_file (Character) - Output file path and file name/type to save the resulting plot (e.g. "c:/temp/output.png") Available file types ("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg")
 #' @param expand_xy (c(Numeric, Numeric)) - Sets expansion amount for the X and Y scale of the map - Vector (expand x, expand y)
 #' @param map_xy_min_max (c(Numeric, Numeric, ...)) - Vector that describes the desired extent of the map in the form of (xmin, xmax, ymin, ymax)
 #' @param map_title (Character) - Title to be displayed on the output map
@@ -30,22 +25,22 @@ library(RColorBrewer)
 #' @param map_legend_title (Character) - Text for the legend header
 #' @param map_x_label (Character) - Label for x axis
 #' @param map_y_label (Character) - Label for y axis
-#' @return (ggplot2) - A ggplot object of the resulting map
+#' @return (ggplot2 or Character) - Returns a ggplot object of the resulting map or an error string if failed
 #' @importFrom sf st_transform
 #' @importFrom raster raster as.data.frame compareCRS minValue maxValue nlayers
 #' @importFrom dplyr mutate
 #' @importFrom ggplot2 scale_x_continuous scale_y_continuous scale_fill_distiller ggplot geom_raster geom_sf coord_sf labs theme geom_sf_label
 #' @importFrom ggspatial layer_spatial df_spatial
+#' @import RColorBrewer
 #' @export
 create_map <- function(shape_data = NULL, shape_label_field = NULL, simplify = FALSE,
                        raster_data = NULL, raster_col = NULL,  raster_band = 1, bin_method = NULL, bins = NULL, convert_zero = FALSE,
-                       dpi = 150, output_file = NULL, output_file_type = "png", expand_xy = c(0, 0),
+                       dpi = 150, output_file = NULL, expand_xy = c(0, 0),
                        map_xy_min_max = c(-180, 180, -90, 90), map_title = NULL,  map_palette = "RdYlBu",
                        map_width_height_in = c(15, 10), map_legend_title = NULL, map_x_label = "Lon", map_y_label = "Lat")
 {
     error <- ""
-    ouput <- "Default error"
-   # browser()
+    output <- "Default error"
     tryCatch(
     {
 
@@ -63,11 +58,9 @@ create_map <- function(shape_data = NULL, shape_label_field = NULL, simplify = F
       # Crop shape - for extent changes (future
       # shape <- st_crop(shape, 1.2*extent(raster))
 
-      # Convert raster
-      raster_df <- df_spatial(raster_obj)
-      #raster_df <- as.data.frame(raster_obj, xy=TRUE)
-
       # Raster operations
+      # Convert raster
+      raster_df <- df_spatial(raster_obj)  #raster_df <- as.data.frame(raster_obj, xy=TRUE) compare performance
       if(is.null(raster_col))
       {
         error <- "No raster column defined"
@@ -76,7 +69,7 @@ create_map <- function(shape_data = NULL, shape_label_field = NULL, simplify = F
       {
         raster_df <- mutate(raster_df, value = raster_df[[paste0("band", 1)]])
       }
-
+#browser()
       # Create mapping and output variables
       # Raster stats/information (future)
       raster_min <- minValue(raster_obj)
@@ -104,7 +97,7 @@ create_map <- function(shape_data = NULL, shape_label_field = NULL, simplify = F
       map_color_scale <-  scale_fill_distiller(palette = map_palette, type = palette_type, direction = palette_direction, na.value = na_value, guide = map_guide)
       if(!is.null(shape_label_field))
       {
-        map_shape_text <- geom_sf_label(data = shape_obj, aes_string(label = shape_label_field, fill=NULL, size = ))
+        map_shape_text <- geom_sf_label(data = shape_obj, aes_string(label = shape_label_field, fill=NULL, size = 1 ))
         #map_shape_text <- geom_text(data = shape_obj, aes(x = x, y = y, label = shape_label_field), size = 4)
       }
       else
@@ -126,7 +119,7 @@ create_map <- function(shape_data = NULL, shape_label_field = NULL, simplify = F
       # Save File
       if(!is.null(output_file))
       {
-        gcammaptools::save_plot(output_file, output_file_type, dpi, map_width, map_height)
+        gcammaptools::save_plot(output_file, dpi, map_width, map_height)
       }
 
     },
@@ -236,17 +229,22 @@ process_raster <- function( raster_data , raster_col, raster_band, bin_method, b
 #'
 #' Initially one line of code, this function is likely to be expanded
 #'
-#' @param output_file (Character) - Output file path to save the resulting plot
-#' @param output_file_type (Character) - Sets file extension type for saving plots. Choices restricted to ("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg" or "wmf)
+#' @param output_file (Character) - Output file path and file name/type to save the resulting plot (e.g. "c:/temp/output.png") Available file types ("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg")
 #' @param dpi (Numeric) - Settable DPI for different print/screen formats
 #' @param map_width (Numeric) - Map width in inches
 #' @param map_height (Numeric) - Map height in inches
 #' @importFrom ggplot2 ggsave
 #' @export
-save_plot <- function(output_file, output_file_type, dpi, map_width, map_height)
-{
-  ggsave(filename = paste0(output_file, ".", output_file_type), device = output_file_type, dpi = dpi, limitsize = TRUE,
-         width = map_width, height = map_height)
+save_plot <- function(output_file, dpi, map_width, map_height)
+{    browser()
+
+  file_type <- substr(x = output_file, start = (nchar(output_file)-2), stop = nchar(output_file))
+  if(file_type %in% c("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg"))
+  {
+    ggsave(filename = output_file, device = file_type, dpi = dpi, limitsize = TRUE,
+           width = map_width, height = map_height)
+  }
+
 }
 # e equal interval, quantile, natural breaks to bin the data into a certain amount of classes as provided by the user
 #test <- create_map(shape_data = "e:/repos/github/data/ri/State_Boundary_1997.shp", raster_data = "E:/Repos/github/data/ri/gaplf2011lc_v30_ri.tif", output_file = "./test", raster_col = "gaplf2011lc_v30_ri_COUNT")
