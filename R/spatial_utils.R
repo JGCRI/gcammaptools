e164c85c4069ac1c1c1977a7451b95f89ea796e8
evanoffPNNL <50331146+evanoffPNNL@users.noreply.github.com>
2020-04-21 12:52:15 -0400

    Updated file name to spatial_utils, added output.r for specific output related functions

--- R/spatial_utils.R
+++ R/spatial_utils.R
@@ -3,7 +3,6 @@
 # Contains general helper functions for the package
 
 
-
 #' Process shape
 #'
 #' @param shape_data (SF, SP, or Character) - Either the full path string to a shape file (with included necessary files) or an SF shape object
@@ -131,44 +130,7 @@ process_raster <- function( raster_data , raster_col, raster_band, bin_method, b
 }
 
 
-#' Handles saving the map output to disk
-#'
-#' Initially one line of code, this function is likely to be expanded
-#'
-#' @param output_file (Character) - Output file path and file name/type to save the resulting plot (e.g. "c:/temp/output.png") Available file types ("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg")
-#' @param dpi (Numeric) - Settable DPI for different print/screen formats
-#' @param map_width (Numeric) - Map width in inches
-#' @param map_height (Numeric) - Map height in inches
-#' @return (Character) - Returns a character string with success or an error string if failed
-#' @importFrom ggplot2 ggsave
-#' @export
-save_plot <- function(output_file, dpi, map_width, map_height)
-{
-  output <- "success"
-  tryCatch(
-    {
-      # Get file type dynamically and save to path
-      file_type <- substr(x = output_file, start = (nchar(output_file)-2), stop = nchar(output_file))
 
-      if(file_type %in% c("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg"))
-      {
-        ggsave(filename = output_file, device = file_type, dpi = dpi, limitsize = TRUE,
-               width = map_width, height = map_height)
-      }
-      else
-      {
-        return("Error: Unrecognized output file type (must be one of eps, ps, tex, pdf, jpeg, tiff, png, bmp, svg")
-      }
-    },
-    error = function(err)
-    {
-      # error handler picks up error information
-      error <- err
-      return(error)
-    })
-
-  return(output)
-}
 
 
 #' Process data
